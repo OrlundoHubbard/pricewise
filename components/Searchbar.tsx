fe5a94e535007d0d7b4bbfdb01018992b9e68844
Orlundo Hubbard <weareprojectcode@gmail.com>
2023-10-21 11:47:25 -0500

    web scraper logic

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,16 +1,43 @@
 "use client"
+import { FormEvent, useState } from "react";
+
+const isValidAmazonProductURL = (url: string) => {
+  try {
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
+
+    // Check if hostname contains amazon.com or amazon.ca
+
+    if  (hostname.includes('amazon.')
+      || hostname.endsWith('amazon')
+    ) {
+      return true;
+    }
+  } catch (error) {
+    return false;
+  }
+
+  return false;
+}
 
 const Searchbar = () => {
-  const handleSubmit = () => {}
+  const [searchPrompt, setSearchPrompt] = useState('');
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const isValidLink = isValidAmazonProductURL(searchPrompt);
+  }
 
   return (
     <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
       <input
-        type= "text"
-        placeholder= "Enter product link"
-        className = "searchbar-input"
-       />
-       <button type="submit" className="searcbar-btn">Search</button>
+        type="text"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
+        placeholder="Enter product link"
+        className="searchbar-input"
+      />
+      <button type="submit" className="searcbar-btn">Search</button>
     </form>
   )
 }
